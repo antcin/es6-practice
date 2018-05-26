3db814066e11871fc34e1f187ec0e433fc9d5e65
antcin <ciniglioantonio@gmail.com>
2018-05-26 10:47:34 +0100

    exercise one, section 2, lecture 6

---
+++ exercise_one_section2_lect6.js
@@ -0,0 +1,19 @@
+//Which of these variables would you change to let or const ?
+
+var name = "James"; // const
+var employeeId = 25; // const
+var eyes = "Blue"; //const
+var age = 25 //let
+var position = "Developer"; //let
+var salary = 2500; //let
+var knowledge = ['Javascript','PHP','REACT','Angular 4']; //let, if he plans to add more, or forget some
+var married = false; //let
+
+const name = "James";
+const employeeId = 25;
+const eyes = "Blue";
+let age = 25;
+let position = "Developer";
+let salary = 2500;
+let knowledge = ['Javascript', 'PHP', 'REACT', 'Angular 4'];
+let married = false;
