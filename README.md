082a0caaf3c2d590c68e87ecdc5cf8e43f636e74
antcin <ciniglioantonio@gmail.com>
2018-05-26 09:53:46 +0100

    const and let

---
+++ README.md
@@ -0,0 +1,1 @@
+# ES6 Practice - [Udemy Tutorial](https://www.udemy.com/the-complete-javascript-es6-course/learn/v4/t/lecture/7836704?start=0)
---
+++ const_let.js
@@ -0,0 +1,14 @@
+// const and let created to make the code more syntatic (more readable)
+
+//ES5
+var type = 'Cupcake';
+var flavor = 'Apple';
+var price = 5;
+
+//ES6
+// const - use whenever we expect the value of the content not to change throughout the entire application
+// let use when we expect the value of the content to change
+
+const type = 'Cupcake'; //plan not to change
+let flavor = 'Apple'; //might change in the future
+let price = 5; //might also change
