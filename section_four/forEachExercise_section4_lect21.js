b75262bba66376ffbbd64c594299c0269163b700
antcin <ciniglioantonio@gmail.com>
2018-05-29 13:38:09 +0100

    phone discount exercise, section 4, lecture 21

---
+++ section_four/forEachExercise_section4_lect21.js
@@ -0,0 +1,36 @@
+// var products = [
+//    {name:'Iphone',price:200},
+//    {name:'Motorola',price:70},
+//    {name:'Samsung',price:150},
+//    {name:'Sony',price:98},
+//    {name:'Windows pone',price:10}
+// ];
+
+const products = [
+   {name:'Iphone',price:200},
+   {name:'Motorola',price:70},
+   {name:'Samsung',price:150},
+   {name:'Sony',price:98},
+   {name:'Windows Phone',price:10}
+];
+
+let template = '';
+
+products.forEach(function(product){
+
+  function discount(){
+    if(product.price < 100){
+      return `<span>On sale !!</span>`
+    }
+  }
+
+  template += `
+  <div class="product">
+   <h1>${product.name}</h1>
+   ${discount()}
+   <strong>Price: $ ${product.price} </strong>
+  </div>
+  `
+});
+
+document.body.insertAdjacentHTML("afterbegin", template)
