ab576e3699087d62f7580c2ef2e030cd2b7bc895
antcin <ciniglioantonio@gmail.com>
2018-05-31 21:50:02 +0100

    fat arrow exercise, distance, section5, lecture 49

---
+++ section_five/fat_arrow_functions_exercise_one.js
@@ -0,0 +1,17 @@
+// //Refactor the code
+// var names= ["James","Ron","Lisa","Tommy"];
+// var randomName = function(items){
+//  return items[Math.floor(Math.random()*items.length)]
+// }
+// var randomNumber = function(maxNumber,minNumber){
+//  return Math.floor(Math.random() * maxNumber) + minNumber;
+// }
+// console.log(randomName(names) + " magic number is " + randomNumber(5,2))
+
+const names = ["James","Ron","Lisa","Tommy"];
+
+let randomName = items => items[Math.floor(Math.random()*items.length)]
+
+let randomNumber = (maxNumber, minNumber) => Math.floor(Math.random() * maxNumber) + minNumber;
+
+console.log(`${randomName(names)}'s magic number is ${randomNumber(5, 2)}`);
