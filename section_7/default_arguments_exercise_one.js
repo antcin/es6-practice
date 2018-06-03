7c3aee32d22acba68eac89800ec3680d0b3481ca
antcin <ciniglioantonio@gmail.com>
2018-06-03 19:15:35 +0100

    default function arguments, exercise one, section 7, lecture 55

---
+++ section_7/default_arguments_exercise_one.js
@@ -0,0 +1,14 @@
+//We want to run a function that greets the user by his name, but if the name is not provided we want to show a default name.
+// function greeting(name){
+//    if(name === undefined){
+//        name = 'user'
+//    }
+//    return 'Hello '+ name;
+// }
+// console.log(greeting())
+
+greeting = (name = 'user' ) => {
+  return `Hello ${name}`
+}
+
+console.log(greeting())
